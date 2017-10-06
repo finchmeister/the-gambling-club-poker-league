7b7ec372ede87de6e7aaf5d627b2381606fa5bb5
Jonathan Finch <thefinchmeister@gmail.com>
2017-10-06 19:56:53 +0100

    Admin controller

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class AdminController
+ * @package AppBundle\Controller
+ * @Route("/admin")
+ */
+class AdminController extends Controller
+{
+    /**
+     * @Route("/", name="admin")
+     */
+    public function indexAction(Request $request)
+    {
+        // TODO
+        return new Response("Admin");
+    }
+}
