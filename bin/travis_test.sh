93f9fbdea331751dea97d7903f3e203295992f92
jfinch <thefinchmeister@gmail.com>
2017-11-23 13:14:28 +0000

    added pause

--- bin/travis_test.sh
+++ bin/travis_test.sh
@@ -5,5 +5,8 @@ docker-compose build
 
 docker-compose up -d
 
-docker-compose exec php ./vendor/phpunit/phpunit/phpunit
+until [ "`/usr/bin/docker inspect -f {{.State.Running}} php`" == "true" ]; do
+    sleep 0.1;
+done;
 
+docker-compose exec php ./vendor/phpunit/phpunit/phpunit
