893ba945e3bba2c1d3c6e5a801f62cbb9971efa7
Jonathan Finch <thefinchmeister@gmail.com>
2017-12-03 19:18:32 +0000

    rearranged commands

--- bin/install-production-docker-machine.sh
+++ bin/install-production-docker-machine.sh
@@ -4,5 +4,3 @@
 curl -L https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
 chmod +x /tmp/docker-machine &&
 sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
-
-./bin/docker-machine-import.sh vm01.zip
