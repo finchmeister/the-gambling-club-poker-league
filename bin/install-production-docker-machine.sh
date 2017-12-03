8830dce94cd89e312adb44860c9286e6e382c757
Jonathan Finch <thefinchmeister@gmail.com>
2017-12-03 18:53:13 +0000

    Travis auto deploy

---
+++ bin/install-production-docker-machine.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Install Docker Machine
+curl -L https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+chmod +x /tmp/docker-machine &&
+sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
+
+./bin/docker-machine-import.sh var/machine/vm01.zip
