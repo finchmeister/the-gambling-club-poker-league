554946b9e8c75da9a0fe7f65ab9a04cec248b79b
jfinch <thefinchmeister@gmail.com>
2018-04-26 18:34:04 +0100

    Response headers

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -13,6 +13,17 @@ class AppExtension extends \Twig_Extension
         );
     }
 
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction(
+                'displayTextAtBreakPoints',
+                [$this, 'displayTextAtBreakPoints',],
+                ['is_safe' => [true]]
+            ),
+        );
+    }
+
     public function priceFilter($number, $decimals = 0, $decPoint = '.', $thousandsSep = ',')
     {
         $price = number_format($number, $decimals, $decPoint, $thousandsSep);
@@ -27,5 +38,18 @@ class AppExtension extends \Twig_Extension
         $price = $price.'%';
 
         return $price;
+    }
+
+    public function displayTextAtBreakPoints(
+        string $textAtLarge,
+        string $textAtSmall,
+        string $breakPoint
+    ) {
+        return sprintf(
+            '<span class="d-none d-%1$s-block">%2$s</span><span class="d-block d-%1$s-none">%3$s</span>',
+            $breakPoint,
+            $textAtLarge,
+            $textAtSmall
+        );
     }
 }
