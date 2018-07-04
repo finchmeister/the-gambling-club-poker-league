2cfaa8147c905269a9e909dffc4fc69a78b249cb
Jonathan Finch <thefinchmeister@gmail.com>
2018-07-04 22:23:38 +0100

    Add missing import

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Game;
 use AppBundle\League\LeagueTableService;
 use AppBundle\PlayerStats\ComputeStats;
 use AppBundle\PokerStats\StatsFactory;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
